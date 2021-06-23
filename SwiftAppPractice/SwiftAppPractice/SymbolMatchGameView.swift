b0f01b5ed687ae8ad38948ca432883b4c2a24bdc
LeeAnn <leewarren2016@gmail.com>
2021-06-23 13:27:01 -0500

    fixed UI appearance
    updates mainly affect SymbolMatchGamView's contents in length and commenting out future fixed bugs

--- SwiftAppPractice/SwiftAppPractice/SymbolMatchGameView.swift
+++ SwiftAppPractice/SwiftAppPractice/SymbolMatchGameView.swift
@@ -11,8 +11,6 @@
 
 import SwiftUI
 
-let lemonYellow = Color(hue: 0.1639, saturation: 1, brightness: 1)
-
 struct SymbolMatchGameView: View {
     @ObservedObject var Game : SymbolMatchGame
     // @ObservedObject means that this body will be rebuilt when changed
@@ -47,20 +45,35 @@ struct SymbolView: View {
     
     var body: some View {
         let shape = Circle()
-        ZStack{
-            if symbol.isFaceUp {
-                shape.fill(Color(.orange)).foregroundColor(.orange)
-                shape.strokeBorder(Color.yellow, lineWidth: 3)
-                shape.overlay(Text(symbol.content).font(.largeTitle) , alignment: .center) //overlay modifier changed the way things stacked, symbol is now on the circle centered
-                    // overlay makes text size to circle whereas .background would make the shape sized to the text and stacked Shape on Text
-                    .padding(.all)
-            } // end if -- if the symbol card is face up, do this
-            else if symbol.isMatched{
-                shape.opacity(0) // like magic
-            }// end else if -- if the symbols are matched, make them dissapear
-            else{
-                shape.overlay(Image("custom.brain").font(.largeTitle) , alignment: .center).foregroundColor(Color("AccentColor"))
-            } // end else -- else just show brain symbol on the card face down
-        }// end zstack
+        GeometryReader(content: { geometry in
+            ZStack{
+                if symbol.isFaceUp {
+                    // commented the two below shape modifiers because they cut off the automatically adjusting overlay <-- find a fix!
+                    //shape.fill(Color(.orange)).foregroundColor(.orange)
+                    shape.strokeBorder(Color.yellow, lineWidth: DrawingConstants.lineWidth)
+                    shape.overlay(Text(symbol.content)
+                                    //.background(Color.yellow) // <-- take all of this background and text stuff and find a way to put it together into the overlay!!!!!!!!!!!!!!!!!!!!!!!!!
+                                    .font(font(in: geometry.size))) //overlay modifier changed the way things stacked, symbol is now on the circle centered with automatically adjusting size symbol
+                        // overlay makes text size to circle whereas .background would make the shape sized to the text and stacked Shape on Text
+                } // end if -- if the symbol card is face up, do this
+                else if symbol.isMatched{
+                    shape.opacity(0) // like magic
+                    // some code here for score later
+                }// end else if -- if the symbols are matched, make them dissapear
+                else{
+                    shape.overlay(Image("custom.brain").font(.largeTitle) , alignment: .center).foregroundColor(Color("AccentColor"))
+                } // end else -- else just show brain symbol on the card face down
+            }// end zstack
+        })// end geomteryReader
     } // end body
+    
+    private func font(in size: CGSize) -> Font {
+        Font.system(size: min(size.width, size.height) * DrawingConstants.fontScale)
+    } // end font func
+    
+    private struct DrawingConstants{
+        static let cornerRadius: CGFloat = 20 // for rectangle
+        static let lineWidth: CGFloat = 3
+        static let fontScale: CGFloat = 0.8
+    } // end DrawingConstants
 } // end SymbolView
